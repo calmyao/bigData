f7003b903a766f7905df68487cfb3d17771c36fc
yaocalm <yaoke0504@163.com>
2019-10-09 21:53:46 +0800

    11

--- src/main/java/com/yao/App.java
+++ src/main/java/com/yao/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
+        System.out.println("1");
         System.out.println( "Hello World!" );
+        System.out.println("2");
     }
 }
